9cb0a08537fbda77320f8ef2aaafdc52e72f7ffa
royal <royalroad91@gmail.com>
2024-04-02 18:22:51 +0900

    add react app

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+function App() {
+    const [hello, setHello] = useState([])
+
+    useEffect(() => {
+        axios.get('/home')
+            .then((response) =>
+            {
+                const object = response.data;
+                console.log(object);
+            })
+            .catch(error => console.log(error))
+    }, []);
+
+    return (
+        <div>
+            백엔드에서 가져온 데이터입니다 : {hello}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/main/frontend/src/setProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+        '/home',
+        createProxyMiddleware({
+            target: 'http://localhost:8080',
+            changeOrigin: true,
+        })
+    );
+};
