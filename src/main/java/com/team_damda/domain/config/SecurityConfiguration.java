af13aa11e2a2c4a524981babdfdb65dccf40efc4
parkjinhee <parkjin931@gmail.com>
2024-04-05 13:38:19 +0900

    CHORE: changed package name

--- src/main/java/com/team_damda/domain/config/SecurityConfiguration.java
+++ src/main/java/com/team_damda/domain/config/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
@@ -38,5 +40,10 @@ public class SecurityConfiguration {
 
         return http.build();
 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
