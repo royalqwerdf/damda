7c8bf7c5d03f6c31decd1bfb21c3098bfe693631
[Cloud2 박수빈] <subin6985@gmail.com>
2024-04-09 17:02:29 +0900

    Feat: 예약 버튼 작동

--- src/main/frontend/src/pages/CartReservationComplete.js
+++
@@ -1,9 +0,0 @@
-import React, {useEffect, useState} from 'react';
-import { Link } from 'react-router-dom';
-import axios from "axios";
-
-function CartReservationComplete() {
-
-}
-
-export default CartReservationComplete;
