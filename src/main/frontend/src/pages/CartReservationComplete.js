fcc7560ee87ee5ad7d9a01fe008bab6f2fc6a20d
[Cloud2 박수빈] <subin6985@gmail.com>
2024-04-09 17:59:27 +0900

    Feat: 메인페이지로 버튼 동작

--- src/main/frontend/src/pages/CartReservationComplete.js
+++ src/main/frontend/src/pages/CartReservationComplete.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../styles/CartReservationComplete.css';
+import { Link } from "react-router-dom";
 
 function CartReservationComplete() {
     return (
@@ -13,7 +14,7 @@ function CartReservationComplete() {
             <div id="row"></div>
             <div id="message">예약이 완료되었습니다!</div>
             <div className="buttons">
-                <button className="mainPage">메인 페이지로</button>
+                <Link to={"/"}><button className="mainPage">메인 페이지로</button></Link>
                 <button className="reservation">내 예약 확인하기</button>
             </div>
         </div>
