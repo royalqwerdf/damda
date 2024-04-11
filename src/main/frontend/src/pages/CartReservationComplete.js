febf57dd3f54407c8ab0216a12994a7ca5e562c3
[Cloud2 박수빈] <subin6985@gmail.com>
2024-04-11 17:14:50 +0900

    Feat: 예약 목록으로 이동하는 버튼

--- src/main/frontend/src/pages/CartReservationComplete.js
+++ src/main/frontend/src/pages/CartReservationComplete.js
@@ -16,7 +16,7 @@ function CartReservationComplete() {
             <div id="message">예약이 완료되었습니다!</div>
             <div className="buttons">
                 <Link to={"/"}><button className="mainPage">메인 페이지로</button></Link>
-                <button className="reservation">내 예약 확인하기</button>
+                <Link to={"/user-reservation"}><button className="reservation">내 예약 확인하기</button></Link>
             </div>
         </div>
     )
