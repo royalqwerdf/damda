3f2b006ee294a590c049d754a18b0ff726460e69
[Cloud2] 이윤희 <yunicode914@gmail.com>
2024-04-12 05:07:03 +0000

    Update App.js

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -6,14 +6,12 @@ import ClassOpen from "./pages/ClassOpen";
 import SearchPage from "./pages/SearchPage";
 import CategoryList from "./pages/CategoryList";
 import CategoryClassList from "./pages/CategoryClassList";
-<<<<<<< HEAD
-import CategoryClassList2 from "./pages/CategoryClassList2";
+
 import UserHome from "./pages/UserHome";
 import AdminHome from "./pages/AdminHome";
-=======
+
 import Inquiry from "./pages/Inquiry";
 import NoticeAndEventPage from "./pages/NoticeAndEventPage";
->>>>>>> 6fd5b42d87c6488291c2bbfdd39050a442eb193c
 
 function App(){
     return(
@@ -24,13 +22,13 @@ function App(){
                     <Route path="/" element={<MainPage/>}></Route>
                     <Route path="/search" element={<SearchPage/>}></Route>
                     <Route path="/class-open" element={<ClassOpen/>}></Route>
-<<<<<<< HEAD
+
                     <Route path="/user-home" element={<UserHome/>}></Route>
                     <Route path="/admin-home" element={<AdminHome/>}></Route>
-=======
+
                     <Route path="/inquiry" element={<Inquiry/>}></Route>
                     <Route path="/news" element={<NoticeAndEventPage/>}></Route>
->>>>>>> 6fd5b42d87c6488291c2bbfdd39050a442eb193c
+
                 </Routes>
                 <Footer />
             </BrowserRouter>
