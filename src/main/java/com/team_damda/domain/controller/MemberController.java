57d99b09ebaad465bdd07e19e03f76385278a451
bellra-jin <parkjin931@gmail.com>
2024-04-14 17:19:37 +0900

    소셜 회원가입 완료페이지 회원이름 노출

--- src/main/java/com/team_damda/domain/controller/MemberController.java
+++ src/main/java/com/team_damda/domain/controller/MemberController.java
@@ -33,11 +33,6 @@ public class MemberController {
      * @return 회원가입
      * @throws Exception
      */
-//    @PostMapping("/signup")
-//    public String signUp(@RequestBody MemberSignupDto memberSignupDto) throws Exception {
-//        memberService.signUp(memberSignupDto);
-//        return "redirect:/memberSaved";
-//    }
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody MemberSignupDto memberSignupDto) throws Exception {
         Member member = memberService.signUp(memberSignupDto);
@@ -65,14 +60,14 @@ public class MemberController {
 
         log.info("핸드폰 번호 업데이트 요청: email={}, phone={}", userEmail, request.getPhone());
         try {
+            // 서버의 Oauth2Signup 메서드 수정 예시
             Member updatedMember = memberService.updateUserPhoneNumber(userEmail, request.getPhone());
-            return ResponseEntity.ok(Map.of("message", "핸드폰 번호가 업데이트 되었습니다: " + updatedMember.getUserEmail()));
+            return ResponseEntity.ok(Map.of("name", updatedMember.getName(), "message", "핸드폰 번호가 업데이트 되었습니다: " + updatedMember.getUserEmail()));
         } catch (Exception e) {
             log.error("사용자 업데이트 중 오류 발생: ", e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "사용자 업데이트 오류"));
         }
     }
-
 
 
 }
