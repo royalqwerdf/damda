8779ae209f43775ec03a74a636ee4146c1e25f53
ddagoo <1886ddagoo@naver.com>
2024-04-16 18:56:22 +0900

    inquiry manage success

--- src/main/java/com/team_damda/domain/repository/InquiryRepositoryImpl.java
+++ src/main/java/com/team_damda/domain/repository/InquiryRepositoryImpl.java
@@ -3,10 +3,13 @@ package com.team_damda.domain.repository;
 import com.team_damda.domain.entity.Inquiry;
 import com.team_damda.domain.enums.Role;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TemporalType;
 import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.security.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -18,6 +21,9 @@ public class InquiryRepositoryImpl implements InquiryRepositoryCustom {
 
     @Override
     public List<Inquiry> searchInquiryByEmail(String classify, String user, String searchContent, Date startDay, Date endDay) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+
         String searchSql = "SELECT i FROM Inquiry i WHERE 1=1";
 
         if (classify != null && !classify.isEmpty()) {
@@ -27,13 +33,14 @@ public class InquiryRepositoryImpl implements InquiryRepositoryCustom {
         if (user != null && !user.isEmpty()) {
             searchSql += " AND i.user_role = :user";
         }
-
+        /*
         if (startDay != null && endDay != null) {
-            searchSql += " AND i.created_at BETWEEN :startDay AND :endDay";
+            searchSql += " AND i.createdAt BETWEEN :startDay AND :endDay";
         }
+         */
 
         if (searchContent != null && !searchContent.isEmpty()) {
-            searchSql += " AND i.user_email LIKE :searchContent";
+            searchSql += " AND i.userEmail LIKE :searchContent";
         }
 
         TypedQuery<Inquiry> query = em.createQuery(searchSql, Inquiry.class);
@@ -46,10 +53,12 @@ public class InquiryRepositoryImpl implements InquiryRepositoryCustom {
             query.setParameter("user", user);
         }
 
+        /*
         if (startDay != null && endDay != null) {
-            query.setParameter("startDay", startDay);
-            query.setParameter("endDay", endDay);
+            query.setParameter("startDay", startDay, TemporalType.TIMESTAMP);
+            query.setParameter("endDay", endDay, TemporalType.TIMESTAMP);
         }
+        */
 
         if (searchContent != null && !searchContent.isEmpty()) {
             query.setParameter("searchContent", "%" + searchContent + "%");
@@ -60,29 +69,24 @@ public class InquiryRepositoryImpl implements InquiryRepositoryCustom {
 
     @Override
     public List<Inquiry> searchAllInquiryByEmail(String classify, String searchContent, Date startDay, Date endDay) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String startDayString = sdf.format(startDay);
+        String endDayString = sdf.format(endDay);
+
         String searchSql = "SELECT i FROM Inquiry i WHERE 1=1";
 
         if (classify != null && !classify.isEmpty()) {
             searchSql += " AND i.type = :classify";
         }
 
-        if (startDay != null && endDay != null) {
-            searchSql += " AND i.created_at BETWEEN :startDay AND :endDay";
-        }
-
         if (searchContent != null && !searchContent.isEmpty()) {
-            searchSql += " AND i.user_email LIKE :searchContent";
+            searchSql += " AND i.userEmail LIKE :searchContent";
         }
 
         TypedQuery<Inquiry> query = em.createQuery(searchSql, Inquiry.class);
 
         if (classify != null && !classify.isEmpty()) {
             query.setParameter("classify", classify);
-        }
-
-        if (startDay != null && endDay != null) {
-            query.setParameter("startDay", startDay);
-            query.setParameter("endDay", endDay);
         }
 
         if (searchContent != null && !searchContent.isEmpty()) {
@@ -94,6 +98,10 @@ public class InquiryRepositoryImpl implements InquiryRepositoryCustom {
 
     @Override
     public List<Inquiry> searchInquiryByTitle(String classify, String user, String searchContent, Date startDay, Date endDay) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String startDayString = sdf.format(startDay);
+        String endDayString = sdf.format(endDay);
+
         String searchSql = "SELECT i FROM Inquiry i WHERE 1=1";
 
         if (classify != null && !classify.isEmpty()) {
@@ -102,10 +110,6 @@ public class InquiryRepositoryImpl implements InquiryRepositoryCustom {
 
         if (user != null && !user.isEmpty()) {
             searchSql += " AND i.user_role = :user";
-        }
-
-        if (startDay != null && endDay != null) {
-            searchSql += " AND i.created_at BETWEEN :startDay AND :endDay";
         }
 
         if (searchContent != null && !searchContent.isEmpty()) {
@@ -122,11 +126,6 @@ public class InquiryRepositoryImpl implements InquiryRepositoryCustom {
             query.setParameter("user", user);
         }
 
-        if (startDay != null && endDay != null) {
-            query.setParameter("startDay", startDay);
-            query.setParameter("endDay", endDay);
-        }
-
         if (searchContent != null && !searchContent.isEmpty()) {
             query.setParameter("searchContent", "%" + searchContent + "%");
         }
@@ -136,14 +135,14 @@ public class InquiryRepositoryImpl implements InquiryRepositoryCustom {
 
     @Override
     public List<Inquiry> searchAllInquiryByTitle(String classify, String searchContent, Date startDay, Date endDay) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String startDayString = sdf.format(startDay);
+        String endDayString = sdf.format(endDay);
+
         String searchSql = "SELECT i FROM Inquiry i WHERE 1=1";
 
         if (classify != null && !classify.isEmpty()) {
             searchSql += " AND i.type = :classify";
-        }
-
-        if (startDay != null && endDay != null) {
-            searchSql += " AND i.created_at BETWEEN :startDay AND :endDay";
         }
 
         if (searchContent != null && !searchContent.isEmpty()) {
@@ -154,11 +153,6 @@ public class InquiryRepositoryImpl implements InquiryRepositoryCustom {
 
         if (classify != null && !classify.isEmpty()) {
             query.setParameter("classify", classify);
-        }
-
-        if (startDay != null && endDay != null) {
-            query.setParameter("startDay", startDay);
-            query.setParameter("endDay", endDay);
         }
 
         if (searchContent != null && !searchContent.isEmpty()) {
